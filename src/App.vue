c90e1358b57521f25d74fe9b2972801bda9fb92c
Lucasinho11 <lucas.lubasinski11@gmail.com>
2023-03-26 17:35:28 +0200

    Added footer component :sparkles:

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,22 @@
 <template>
-  <nav>
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
-  </nav>
-  <router-view />
+  <div class="container">
+    <nav>
+      <router-link to="/">Home</router-link> |
+      <router-link to="/about">About</router-link>
+    </nav>
+    <Footer />
+    <router-view />
+  </div>
 </template>
 
 <script>
 import "@/assets/css/app.css";
+import Footer from "@/components/Footer.vue";
 
 export default {
   name: "App",
+  components: {
+    Footer,
+  },
 };
 </script>
---
+++ src/components/Footer.vue
@@ -0,0 +1,52 @@
+<template>
+  <footer class="fixed-bottom bg-dark text-light mb-5">
+    <navbar class="container d-flex justify-content-between">
+      <div class="d-flex justify-content-center align-items-start">
+        <p>© 2023 GitEmoji</p>
+        <span class="dot"></span>
+        <p>Made with ❤️️ by Thomas & Lucas</p>
+      </div>
+      <div class="d-flex justify-content-center align-items-start">
+        <a
+          href="https://github.com/thomaseyaa/gitemoji"
+          class="text-light text-decoration-none"
+          >About</a
+        >
+        <span class="dot"></span>
+        <a
+          href="https://github.com/thomaseyaa/gitemoji"
+          class="text-light text-decoration-none"
+          >Contributors</a
+        >
+        <span class="dot"></span>
+        <a
+          href="https://github.com/thomaseyaa/gitemoji"
+          class="text-light text-decoration-none"
+          >GitHub</a
+        >
+      </div>
+    </navbar>
+  </footer>
+</template>
+
+<script>
+export default {
+  // eslint-disable-next-line vue/multi-word-component-names
+  name: "Footer",
+  props: {
+    msg: String,
+  },
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+.dot {
+  height: 3px;
+  width: 3px;
+  background-color: white;
+  border-radius: 50%;
+  display: inline-block;
+  margin: 10px;
+}
+</style>
