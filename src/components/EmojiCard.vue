428767912c2f8f1bc833166def5b04085ab37d11
Lucasinho11 <lucas.lubasinski11@gmail.com>
2023-03-27 18:26:32 +0200

    Added toast feature :sparkles:

--- src/components/EmojiCard.vue
+++ src/components/EmojiCard.vue
@@ -3,6 +3,7 @@
     class="card py-5 text-center mb-3 col-3"
     @click="copyText(item.shortcode)"
   >
+    <Toast v-if="copied" :message="text" />
     <span class="dot"></span>
     <span style="font-size: 110px">{{ item.emoji }}</span>
     <span style="font-size: 30px">{{ item.shortcode }}</span>
@@ -11,15 +12,32 @@
 </template>
 
 <script>
+import Toast from "@/components/Toast.vue";
+
 export default {
   // eslint-disable-next-line vue/multi-word-component-names
   name: "EmojiCard",
+  components: {
+    Toast,
+  },
   props: {
     item: Object,
   },
+  data() {
+    return {
+      copied: false,
+      text:
+        "Shortcode " + this.item.shortcode + " " + this.item.emoji + " copied!",
+    };
+  },
   methods: {
     copyText(item) {
+      this.copied = false;
       navigator.clipboard.writeText(item);
+      this.copied = true;
+      setTimeout(() => {
+        this.copied = false;
+      }, 100); // set the timeout to 2 seconds (adjust as needed)
     },
   },
 };
--- src/main.js
+++ src/main.js
@@ -1,6 +1,8 @@
 import { createApp } from "vue";
 import App from "./App.vue";
 import router from "./router";
+import Toast from "vue-toastification";
+import "vue-toastification/dist/index.css";
 import store from "./store";
 
-createApp(App).use(store).use(router).mount("#app");
+createApp(App).use(store).use(Toast).use(router).mount("#app");
