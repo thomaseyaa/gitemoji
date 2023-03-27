e78b19b72799e4f13da6656f2771578dd03e18a9
Lucasinho11 <lucas.lubasinski11@gmail.com>
2023-03-27 16:20:56 +0200

    Added pages buttons :sparkles:

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -13,6 +13,29 @@
         />
       </div>
     </div>
+    <nav class="d-flex justify-content-center mt-5">
+      <ul class="pagination">
+        <li class="page-item" @click="previousPage()">
+          <a class="page-link text-white" href="#" aria-label="Previous">
+            <span aria-hidden="true">&laquo;</span>
+          </a>
+        </li>
+        <li class="page-item" @click="changePageWithNb(0)">
+          <a class="page-link text-white" href="#">1</a>
+        </li>
+        <li class="page-item" @click="changePageWithNb(1)">
+          <a class="page-link text-white" href="#">2</a>
+        </li>
+        <li class="page-item" @click="changePageWithNb(2)">
+          <a class="page-link text-white" href="#">3</a>
+        </li>
+        <li class="page-item" @click="nextPage()">
+          <a class="page-link text-white" href="#" aria-label="Next">
+            <span aria-hidden="true">&raquo;</span>
+          </a>
+        </li>
+      </ul>
+    </nav>
   </div>
 </template>
 
@@ -40,6 +63,21 @@ export default {
     async getEmojis() {
       await this.getData();
       this.items = this.emojisData;
+    },
+    changePageWithNb(nb) {
+      this.currentPage = nb;
+    },
+    previousPage() {
+      const totalLength = Object.keys(this.items).length;
+      if (this.currentPage < totalLength - 1 && this.currentPage > 0) {
+        this.currentPage--;
+      }
+    },
+    nextPage() {
+      const totalLength = Object.keys(this.items).length;
+      if (this.currentPage < totalLength - 1 && this.currentPage >= 0) {
+        this.currentPage++;
+      }
     },
   },
   mounted() {
