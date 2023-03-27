51fd77819328c1f8e2f87c0935c857c80dd78f2b
Thomas Eyaa <thomasfkey@gmail.com>
2023-03-27 15:00:22 +0200

    Store config 🏗️

--- src/store/actions.js
+++ src/store/actions.js
@@ -1,10 +1,36 @@
 import data from "@/assets/json/emojis.json";
+import store from "../store";
 
-export async function getData() {
-  return data;
-}
+export const getData = async () => {
+  paginate(data).then((data) => {
+    store.commit("emojisData", {
+      ...data,
+    });
+  });
+};
 
-export async function searchData(query) {
-  console.log(data.filter((emoji) => emoji.description.includes(query)));
-  return data.filter((emoji) => emoji.description.includes(query));
-}
+// eslint-disable-next-line no-empty-pattern
+export const searchData = async ({}, value) => {
+  const search = value.toLowerCase();
+  const items = data.filter(
+    (emoji) => emoji.description.includes(search) || emoji.name.includes(search)
+  );
+  paginate(items).then((data) => {
+    store.commit("emojisData", {
+      ...data,
+    });
+  });
+};
+
+export const paginate = async (items) => {
+  const pageSize = 16;
+  const pageCount = Math.ceil(items.length / pageSize);
+  const pages = [];
+
+  for (let i = 0; i < pageCount; i++) {
+    const start = i * pageSize;
+    const end = start + pageSize;
+    pages.push(items.slice(start, end));
+  }
+  return pages;
+};
