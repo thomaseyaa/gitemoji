4ab00fda6ba9be443e67286adf1571140ad2e887
Lucasinho11 <lucas.lubasinski11@gmail.com>
2023-03-27 15:49:49 +0200

    Added copy shortcode feature :sparkles:

--- src/components/EmojiCard.vue
+++ src/components/EmojiCard.vue
@@ -1,51 +1,26 @@
 <template>
-  <div class="container flex-wrap">
-    <div v-for="(page, index) in items" :key="index">
-      <div
-        v-if="index == currentPage"
-        class="row d-flex justify-content-between"
-      >
-        <div
-          v-for="item in page"
-          :key="item.name"
-          class="card py-5 text-center mb-3 col-3"
-        >
-          <span class="dot"></span>
-          <span style="font-size: 110px">{{ item.emoji }}</span>
-          <span style="font-size: 30px">{{ item.shortcode }}</span>
-          <span style="font-size: 20px">{{ item.description }}</span>
-        </div>
-      </div>
-    </div>
+  <div
+    class="card py-5 text-center mb-3 col-3"
+    @click="copyText(item.shortcode)"
+  >
+    <span class="dot"></span>
+    <span style="font-size: 110px">{{ item.emoji }}</span>
+    <span style="font-size: 30px">{{ item.shortcode }}</span>
+    <span style="font-size: 20px">{{ item.description }}</span>
   </div>
 </template>
 
 <script>
-import { mapActions, mapGetters } from "vuex";
-
 export default {
   // eslint-disable-next-line vue/multi-word-component-names
   name: "EmojiCard",
-  data() {
-    return {
-      items: [],
-      currentPage: 0,
-      receivedData: null,
-    };
-  },
-  computed: {
-    ...mapGetters(["emojisData"]),
+  props: {
+    item: Object,
   },
   methods: {
-    ...mapActions(["getData"]),
-
-    async getEmojis() {
-      await this.getData();
-      this.items = this.emojisData;
+    copyText(item) {
+      navigator.clipboard.writeText(item);
     },
-  },
-  mounted() {
-    this.getEmojis();
   },
 };
 </script>
@@ -58,6 +33,7 @@ export default {
   height: 21rem;
   padding: 10px;
   background-color: #253656;
+  cursor: pointer;
 }
 .dot {
   position: absolute;
