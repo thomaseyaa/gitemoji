45bff9bba975522d5cbd054e22eeb10ba540999b
Thomas Eyaa <thomaseyaa.pro@gmail.com>
2024-10-27 16:51:36 +0100

    Added buymeacoffee link in footer :money_with_wings:

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -1,12 +1,17 @@
 <template>
-  <nav class="navbar container mb-5">
-    <div class="text-center d-flex justify-content-center mobile">
-      <p>
-        © 2023
+  <nav
+    class="navbar container d-flex flex-column flex-md-row align-items-center mb-5 text-center text-md-start"
+  >
+    <div
+      class="d-flex flex-column flex-md-row col-12 col-md-4 justify-content-center justify-content-md-start align-items-center"
+    >
+      <span class="mb-4 mb-md-0">
+        ©
+        {{ new Date().getFullYear() }}
         <a href="/" class="text-light text-decoration-none">GitEmoji</a>
-      </p>
-      <span class="dot d-none d-md-block"></span>
-      <p>
+      </span>
+      <span class="dot d-none d-md-block mx-3"></span>
+      <span>
         Made with ❤️️ by
         <a
           class="text-light text-decoration-none"
@@ -21,23 +26,33 @@
           target="_blank"
           >Lucas</a
         >
-      </p>
+      </span>
     </div>
-    <div class="d-flex justify-content-center">
+    <div class="col-12 col-md-4 text-center py-4 py-md-0">
+      <a href="https://www.buymeacoffee.com/gitemoji" target="_blank"
+        ><img
+          src="https://cdn.buymeacoffee.com/buttons/v2/default-yellow.png"
+          alt="Buy Me A Coffee"
+          style="height: 60px !important; width: 217px !important"
+      /></a>
+    </div>
+    <div
+      class="d-flex col-12 col-md-4 justify-content-center justify-content-md-end"
+    >
       <a
         href="https://github.com/thomaseyaa/gitemoji#about"
         class="text-light text-decoration-none"
         target="_blank"
         >About</a
       >
-      <span class="dot"></span>
+      <span class="dot d-none d-md-block mx-3"></span>
       <a
         href="https://github.com/thomaseyaa/gitemoji/graphs/contributors"
         class="text-light text-decoration-none"
         target="_blank"
         >Contributors</a
       >
-      <span class="dot"></span>
+      <span class="dot d-none d-md-block mx-3"></span>
       <a
         href="https://github.com/thomaseyaa/gitemoji"
         class="text-light text-decoration-none"
@@ -66,10 +81,6 @@ export default {
 }
 
 @media screen and (max-width: 768px) {
-  nav {
-    flex-direction: column;
-    justify-content: space-between;
-  }
   .mobile {
     flex-direction: column;
   }
