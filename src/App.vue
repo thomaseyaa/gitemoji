6b49e163acc338e885552f784aae2b784d6a6692
Thomas Eyaa <thomaseyaa.pro@gmail.com>
2025-03-07 10:11:47 +0100

    Added Vercel Speed Insights for performance tracking 🚀

--- src/App.vue
+++ src/App.vue
@@ -2,17 +2,20 @@
   <div class="px-4">
     <router-view />
     <Footer />
+    <SpeedInsights />
   </div>
 </template>
 
 <script>
 import "@/assets/css/app.min.css";
 import Footer from "@/components/Footer.vue";
+import { SpeedInsights } from "@vercel/speed-insights/vue";
 
 export default {
   name: "App",
   components: {
     Footer,
+    SpeedInsights,
   },
 };
 </script>
